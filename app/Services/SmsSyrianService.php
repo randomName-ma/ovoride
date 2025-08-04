5f33421e1b93541854edf0eb7a99babfebcdd703
ABDALRZAQ345 <jmalj6564@gmail.com>
2025-08-04 16:48:03 +0300

    fix all numbers forms

--- app/Services/SmsSyrianService.php
+++ app/Services/SmsSyrianService.php
@@ -28,12 +28,14 @@ class SmsSyrianService
         $user->ver_code         = verificationCode(6);
         $user->ver_code_send_at = Carbon::now();
         $user->save();
-
-        $this->sendSMS('+963'.$mobile,$user->ver_code,'your code in mishwar is');
+        $phone = $this->normalizeSyrianPhoneNumber($mobile); // <-- Normalize before use
+        $this->sendSMS($phone,$user->ver_code,'your code in mishwar is');
     }
 
     public function sendSMS($phone, $otp, $message)
     {
+
+        \Log::info($phone);
         $requestBody = [
             'message' => sprintf('%s %d', $message, $otp),
             'to' => $phone,
@@ -53,5 +55,24 @@ class SmsSyrianService
         } catch (RequestException $e) {
             throw new \Exception('Failed to send SMS: ' . $e->getMessage());
         }
+    }
+    protected function normalizeSyrianPhoneNumber(string $phone): string
+    {
+        // Remove all non-numeric characters except the plus sign
+        $phone = preg_replace('/[^0-9+]/', '', $phone);
+
+        // Normalize the number
+        if (preg_match('/^\+9639\d{8}$/', $phone)) {
+            return $phone;
+        } elseif (preg_match('/^09\d{8}$/', $phone)) {
+            return '+963' . substr($phone, 1);
+        } elseif (preg_match('/^9\d{8}$/', $phone)) {
+            return '+963' . $phone;
+        } elseif (preg_match('/^9639\d{8}$/', $phone)) {
+            return '+' . $phone;
+        }
+
+        // If it's invalid, throw an exception
+        throw new \InvalidArgumentException("رقم الهاتف غير صالح: $phone");
     }
 }
